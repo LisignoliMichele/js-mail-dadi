f36aa72b47b6e107971518b5eff1605a88cc63de
Michele Lisignoli <michele@lisignoli.com>
2021-03-01 17:35:41 +0100

    dadi

---
+++ dadi/js/script.js
@@ -0,0 +1,16 @@
+// user number from 1  to 6
+var userNumber = prompt("Enter a number from 1 to 6");
+if (userNumber < 1 || userNumber > 6) {
+  userNumber = prompt("The number isn't valid, please try again.. Enter a number from 1 to 6");
+}
+// computer random number from 1  to 6
+var computerNumber = Math.floor(Math.random() * 6) +1;
+var message = "the computer drew the number " + computerNumber;
+// who's the winner
+if ( userNumber == computerNumber ) {
+  alert( "The result of the game is Even " + message );
+}if ( userNumber > computerNumber ) {
+  alert( "You Won " + message );
+}else{
+  alert( "You Lost " + message );
+}
